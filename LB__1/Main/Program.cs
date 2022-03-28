611a4b77ba0ae4be722955dda5cdbb5cf4ab56fe
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2022-03-28 11:33:51 +0300

    +++

--- LB__1/Main/Program.cs
+++ LB__1/Main/Program.cs
@@ -1,15 +1,123 @@
-﻿using System;
+﻿//                                         Лабораторная работа №1
+//                                                Вариант 4
+
+//Ввести  целочисленный двухмерныйрваный  массив  (jagged  array), состоящий из  строк  произвольной  длины. 
+//После  строк  с  максимальной суммой элементов вставить копию строки с минимальной суммой элементов. 
+//Реализовать функцию вычисления суммы строки.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Main
+namespace LB__1
 {
-    class Program
+    internal class Program
     {
+        
+        static int RowSum(int[][] arr, int n)             //\\ //\\  Функция суммирования строки //\\ //\\
+        {
+            int sum = 0;                                  //Переменная для суммы строки
+            for (int i = 0; i < arr[n].Length; i++)      //Перебор строки по n-нному элементу
+                sum += arr[n][i];                       //Накопление
+            return sum;                                //Возращение переменной
+        }
+
+        static void AddArr(int[][] arr, int count)      //\\ //\\  Функция заполнения ступенчатого массива //\\ //\\
+        {
+            for (int i = 0; i < count; i++)                      
+            {
+                Console.WriteLine("Введите " + i + " строку (ввод выполняйте через пробел!)");
+                string str = Console.ReadLine();                  //Ввод в консоль значений
+                string[] str_arr = str.Split();                  //Разбитие и присваивание значений в массив
+
+                arr[i] = new int[str_arr.Length];              //В массив заносим кол-во строк
+                for (int j = 0; j < arr[i].Length; j++)       //Перебираем исходя из кол-ва стр. в массиве
+                    arr[i][j] = int.Parse(str_arr[j]);       //Делаем переобразование в Int и заносим в массив
+            }
+        }
+
+        static void WriteArr(int[][] arr)              //\\ //\\  Функция вывода ступенчатого массива //\\ //\\
+        {
+            for (int i = 0; i < arr.Length; i++)                 //Перебираем исходя из кол-ва стр. в массиве
+            {
+                for (int j = 0; j < arr[i].Length; j++)        //Перебираем исходя из кол-ва элем. в массиве
+                    System.Console.Write(arr[i][j] + " ");    //Выводим на консоль ступенчатый массив, через пробел 
+                System.Console.WriteLine();                  //После вывода n-ой строки 
+            }
+            System.Console.WriteLine();
+        }
+
+        static void FunArr(int[][] arr, int[] minRow, int[] maxRow)     //\\ //\\ Функция для нахождения Макс. и Мин. //\\ //\\
+        {
+            int min = RowSum(arr, 0);                       //Переменная с минимальной суммой строки
+            int max = RowSum(arr, 0);                      //Переменная с максимальнй суммой строки
+
+            for (int i = 1; i < arr.Length; i++)         //Перебираем строки
+            {
+                int tempMin = RowSum(arr, i);          //Доп. переменная для замен
+                if (min > tempMin)                    //Если Мин. больше Доп.переменной идет замена
+                {
+                    min = tempMin;
+                    minRow = arr[i];
+                }
+                int tempMax = RowSum(arr, i);     //Доп. переменная для замен
+                if (max < tempMax)               //Если Макс. меньше Доп.переменной идет замена
+                {
+                    max = tempMax;
+                    maxRow = arr[i];
+                }
+            }
+        }
+
+        static int [][] ModifFunArr(int[][] arr, int[] minRow, int[] maxRow, int a)       //\\ //\\ Функция изменения массива //\\ //\\
+        {
+            int[][] arr2 = new int[a + 1][];                           //Создали новый рванный массив добавляем +1 место для новой строки
+            for (int i = 0; i < arr.Length; i++)                      //Перебираем строки
+            {
+                if (maxRow == arr[i])                               //Если строка с максимальной суммой элементов ровняется строке, то вставить копию строки с минимальной суммой элементов. 
+                {
+                    arr2[i] = arr[i];                             //Перенесли данные в новый массив
+                    arr2[i + 1] = minRow;                        //Перемещаем +1 строку и вставляем мин.строку
+                    i++;
+                    for (int j = i + 1; j < arr2.Length; j++)
+                        arr2[j] = arr[j - 1];
+                    break;
+                }
+                else
+                    arr2[i] = arr[i];                      //Перемещаем данные в новый массив
+            }
+
+            arr = arr2;                                 //Переносим данные с нового массива в основой массив      
+            return arr;                                //Возвращаем в Main
+        }
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Введите количество строк массива:");
+            int a = Convert.ToInt32(Console.ReadLine());                     //Запрашиваем кол. строк
+            int[][] arr = new int[a][];                                     //Создали рванный массив и присволили кол. строк
+
+            AddArr(arr, a);                                               //Переходим в функ-ю заполнения строк
+
+            Console.WriteLine("\nИзначальный массив:");
+            WriteArr(arr);                                              //Функ-я вывода
+
+            int[] minRow = arr[0];                                    //Ставим поумолчанию мин строку массива
+            int[] maxRow = arr[0];                                   //Ставим поумолчанию макс строку массива
+
+            FunArr(arr, minRow, maxRow);                           //Переходим в функ-ю для нахождения макс. и мин. строки
+
+           arr = ModifFunArr(arr, minRow, maxRow, a);            //Переходим в функ-ю модернизации рванного массива
+
+            //вывод измененного исходного массива
+            System.Console.WriteLine("Измененный массив:");
+            WriteArr(arr);                                    //Функ-я вывода
+
+
+            System.Console.ReadKey();
         }
     }
 }
---
+++ LB__1/Test/Program.cs
@@ -0,0 +1,225 @@
+﻿/* 4. Ввести целочисленный двухмерный рваный массив (jagged array),
+состоящий из строк произвольной длины. После строк с максимальной
+суммой элементов вставить копию строки с минимальной суммой элементов.
+Реализовать функцию вычисления суммы строки.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        //нахождение суммы в строке
+        public static int RowSum(int[][] arr, int n)
+        {
+            int a = 0;
+            for (int i = 0; i < arr[n].Length; i++)
+                a += arr[n][i];
+            return a;
+        }
+
+        static int sum(int[] a)  // сумма елементов
+        {
+
+            int s = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                s += a[i];
+            }
+
+            return s;
+
+        }
+
+        static void Sort(int [][]arr, int a)
+        {
+            //int[][] arr2 = new int[a][];
+
+            //int buffer;
+            //for (int lineNum = 0; lineNum < arr.Length; lineNum++)
+            //    for (int elementNum = 0; elementNum < arr[lineNum].Length; elementNum++)
+            //        for (int k = elementNum + 1; k < arr[lineNum].Length; k++)
+            //            if (arr[lineNum][elementNum] > arr[lineNum][k])
+            //            {
+            //                buffer = arr[lineNum][elementNum];
+            //                arr[lineNum][elementNum] = arr[lineNum][k];
+            //                arr[lineNum][k] = buffer;
+            //            }
+            for (int n = 0; n < arr.Length; n++)
+            {
+                for (int i = 1; i < arr.Length; i++) //проходимся по столбцам, начинаем с первого
+                {
+                    int sumPrevRows = 0; // сумма предыдущего столбца
+                    for (int j = 0; j < arr[i].Length; j++)
+                    {
+                        sumPrevRows += arr[i-1][j];
+                    }
+
+                    int sumCurrentRows = 0;       // сумма текущего столбца
+                    for (int j = 0; j < arr[i].Length; j++)
+                    {
+                        sumCurrentRows += arr[i][j];
+                    }
+
+                    if (sumPrevRows > sumCurrentRows) //если сумма предыдущего столбца больше чем текущего, то меняем местами столбцы
+                    {
+                        for (int k = 0; k < arr.Length; k++)
+                        {
+                            int temp = arr[k][i];
+                            arr[k][i] = arr[k][i - 1];
+                            arr[k][i - 1] = temp;
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            //запрос на ввод количества строк массива
+            Console.WriteLine("Введите количество строк массива:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[][] arr = new int[a][];
+
+            //ввод чисел в массив
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine("Введите " + i + " строку (каждое число через пробел):");
+                string s = Console.ReadLine();
+                string[] s_arr = s.Split();
+
+                arr[i] = new int[s_arr.Length];
+                for (int j = 0; j < arr[i].Length; j++)
+                    arr[i][j] = int.Parse(s_arr[j]);
+            }
+
+            //вывод массива
+            Console.WriteLine("\nИзначальный массив:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                    System.Console.Write(arr[i][j] + " ");
+                System.Console.WriteLine();
+            }
+            System.Console.WriteLine();
+
+            Sort(arr, a);
+            //int buffer;
+            //for (int lineNum = 0; lineNum < arr.Length; lineNum++)
+            //    for (int elementNum = 0; elementNum < arr[lineNum].Length; elementNum++)
+            //        for (int k = elementNum + 1; k < arr[lineNum].Length; k++)
+            //            if (arr[lineNum][elementNum] > arr[lineNum][k])
+            //            {
+            //                buffer = arr[lineNum][elementNum];
+            //                arr[lineNum][elementNum] = arr[lineNum][k];
+            //                arr[lineNum][k] = buffer;
+            //            }
+
+
+            //нахождение минимальной и максимальной строки
+
+            //int max = RowSum(arr, 0);
+            //int[] maxRow = arr[0];
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    int tempMin = RowSum(arr, i);
+            //    if (min > tempMin)
+            //    {
+            //        min = tempMin;
+
+            //        for (i = 0; i < arr[a].Length; i++)
+            //        {
+            //            minRow = arr[i];
+            //            arr[i] = arr[a - 1];
+            //            arr[a - 1] = minRow;
+            //        }
+
+            //    }
+            //    //int tempMax = RowSum(arr, i);
+            //    //if (max < tempMax)
+            //    //{
+            //    //    max = tempMax;
+            //    //    maxRow = arr[i];
+            //    //}
+            //}
+
+            //запись изменений в новый массив
+            //int[][] arr2 = new int[a][];
+            //int min = RowSum(arr, 0);
+            //int[] minRow = arr[0];
+
+            //for (int i = 1; i < arr.Length; i++)
+            //{
+            //    int tempMin = RowSum(arr, i);
+            //    int secondSum = RowSum(arr, i + 1);
+
+            //    int[] secondRow = arr[i];
+
+            //    if (min > tempMin)
+            //    {
+            //        min = tempMin;
+            //        minRow = arr[i];
+            //        arr2[i] = minRow;
+            //        minRow = arr[i];
+            //        arr[i] = minRow;
+            //    }
+
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    for (int j = i + 1; j < arr[i].Length; j++)
+            //    {
+
+            //        int secondSum = RowSum(arr, i + 1);
+            //        int[] secondRow = arr[i];
+            //        if (min > secondSum)
+            //        {
+
+
+            //            arr2[i] = arr[i];
+            //            arr[i] = arr[i + 1];
+            //            arr[i + 1] = arr2[i];
+
+
+
+            //            //arr2[i] = arr[i];
+            //            //arr2[i + 1] = minRow;
+
+            //        }
+            //        else if (secondSum > min)
+            //        {
+            //            arr2[i] = arr[i];
+            //            arr[i] = arr[i + 1];
+            //            arr[i + 1] = arr2[i];
+
+            //            min = secondSum;
+            //        }
+            //    }
+            //}
+
+            //изменение исходного массива
+            //arr = arr2;
+
+            //вывод измененного исходного массива
+            System.Console.WriteLine("Измененный массив:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                    System.Console.Write(arr[i][j] + " ");
+                System.Console.WriteLine();
+            }
+            System.Console.WriteLine();
+
+
+            System.Console.ReadKey();
+        }
+    }
+}
